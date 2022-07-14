1bfaa0a209124fc9e677cccddcaa019d2414706d
Alexey <a.yasinko@mail.ru>
2022-07-15 02:56:30 +0300

    Control completed

--- Assets/CameraBoundsController.cs
+++ Assets/CameraBoundsController.cs
@@ -9,6 +9,7 @@ public class CameraBoundsController : MonoBehaviour
     public CinemachineVirtualCamera cineCamera;
 
     private float cineXValue;
+    private bool componentAdded = false;
     private CinemachineComposer transposer;
 
     private void Awake()
@@ -18,7 +19,27 @@ public class CameraBoundsController : MonoBehaviour
 
     private void Update()
     {
+        DoParralax();
+        StopTrackingSpaceShipPosition();
+    }
+
+    private void DoParralax()
+    {
         cineXValue = (transform.position.x - -5) / (5 + 5);
         transposer.m_ScreenX = Mathf.Clamp(Mathf.Lerp(transposer.m_ScreenX, cineXValue, Time.deltaTime), 0.45f, 0.55f);
+    }
+
+    private void StopTrackingSpaceShipPosition()
+    {
+        if ((transform.position.x > boundValue || transform.position.x < -boundValue) && componentAdded == false)
+        {
+            componentAdded = true;
+            cineCamera.gameObject.AddComponent<LockCameraZ>().m_ZPosition = cineCamera.transform.position.x;
+        }
+        else if (((transform.position.x < boundValue && transform.position.x > -boundValue) || (transform.position.x > -boundValue && transform.position.x < boundValue)) && componentAdded == true)
+        {
+            componentAdded = false;
+            Destroy(cineCamera.gameObject.GetComponent<LockCameraZ>());
+        }
     }
 }
--- Assets/SpaceShipMovement.cs
+++ Assets/SpaceShipMovement.cs
@@ -4,46 +4,40 @@ using UnityEngine;
 
 public class SpaceShipMovement : MonoBehaviour
 {
-    // public float verticalForce;
     public float constantVelocity;
     public float rotationDamping;
     public int sideSpeed;
+    public Joystick joystick;
+    public Transform shuttleModel;
 
     private Rigidbody spaceRB;
+    private Quaternion startAngle;
     private Quaternion needAngle;
-    public Joystick joystick;
-
-    public Transform shuttleModel;
 
     private void Awake()
     {
         spaceRB = GetComponent<Rigidbody>();
+        startAngle = shuttleModel.rotation;
     }
 
     private void FixedUpdate()
     {
         spaceRB.velocity = new Vector3(joystick.Horizontal * sideSpeed, constantVelocity, 0);
-
-        // Vector3 direction = new Vector3(joystick.Horizontal * sideSpeed, constantVelocity, 0);
-        // spaceRB.MovePosition(transform.position + new Vector3(joystick.Horizontal * sideSpeed, transform.position.y, 0) * Time.fixedDeltaTime);
         Heeling();
-
-        // Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, 0, -joystick.Horizontal * rotationDamping) * Time.fixedDeltaTime);
-        // spaceRB.MoveRotation(spaceRB.rotation * deltaRotation);
     }
 
     private void Heeling()
     {
         if (joystick.Horizontal != 0)
         {
-            needAngle = Quaternion.Euler(0, 180 + joystick.Horizontal * -30, 0);
+            needAngle = Quaternion.Euler(0, 180 + joystick.Horizontal * -15, joystick.Horizontal * 30);
+            shuttleModel.rotation = Quaternion.Slerp(shuttleModel.rotation, needAngle, Time.deltaTime * rotationDamping);
         }
 
         if (joystick.Horizontal == 0)
         {
-            needAngle = shuttleModel.rotation;
+            needAngle = startAngle;
+            shuttleModel.rotation = Quaternion.Slerp(shuttleModel.rotation, needAngle, Time.deltaTime * rotationDamping);
         }
-        shuttleModel.rotation = Quaternion.Slerp(shuttleModel.rotation, needAngle, Time.deltaTime * rotationDamping);
-
     }
 }
