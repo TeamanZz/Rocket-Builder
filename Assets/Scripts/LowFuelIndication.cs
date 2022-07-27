fac0150b44ff556b88bca1e97f52015b94ab1d7c
Alexey <a.yasinko@mail.ru>
2022-07-27 17:50:50 +0300

    out of fuel + clean

--- Assets/Scripts/LowFuelIndication.cs
+++ Assets/Scripts/LowFuelIndication.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 using TMPro;
 
 public class LowFuelIndication : MonoBehaviour
 {
-    public TextMeshProUGUI lowFuelText;
+    public CanvasGroup canvasGroup;
     private void OnEnable()
     {
-        lowFuelText.DOFade(1, 1).SetLoops(-1, LoopType.Yoyo);
+        canvasGroup.DOFade(1, 0.8f).SetLoops(-1, LoopType.Yoyo);
     }
 }
